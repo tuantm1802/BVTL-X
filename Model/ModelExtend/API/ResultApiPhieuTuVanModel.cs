602c60c4c016995569d19a7314942d76955290bd
VuVanLai <lai.bkhn123@gmail.com>
2022-10-01 11:25:50 +0700

    laivv

---
+++ Model/ModelExtend/API/ResultApiPhieuTuVanModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ModelExtend.API
+{
+    public class ResultApiPhieuTuVanModel
+    {
+        public string record_id { get; set; }
+        public string ngaynhap { get; set; }
+        public string ngaytuvan { get; set; }
+        public string diadiem { get; set; }
+        public string matcv { get; set; }
+        public string makh { get; set; }
+        public string lantuvan { get; set; }
+        public string cau1_1 { get; set; }
+        public string cau1_1k { get; set; }
+        public string cau1_2 { get; set; }
+        public string cau1_3 { get; set; }
+        public string cau1_4 { get; set; }
+        public string cau1_5 { get; set; }
+        public string cau2 { get; set; }
+        public string cau2_1k { get; set; }
+        public string cau2_2 { get; set; }
+        public string cau3 { get; set; }
+        public string cau3_1k { get; set; }
+        public string cau3_1k_2 { get; set; }
+        public string cau4 { get; set; }
+        public string cau4_1k { get; set; }
+        public string cau4_1k_2 { get; set; }
+        public string cau5_1 { get; set; }
+        public string cau5_1k { get; set; }
+        public string cau5_1_2 { get; set; }
+        public string cau5_2 { get; set; }
+        public string cau5_2k { get; set; }
+        public string cau5_2k_2 { get; set; }
+        public string cau5_3 { get; set; }
+        public string cau5_3_1 { get; set; }
+        public string cau5_4 { get; set; }
+        public string cau5_4_1 { get; set; }
+        public string cau5_5 { get; set; }
+        public string cau5_4_2 { get; set; }
+        public string cau5_6 { get; set; }
+        public string cau5_6k { get; set; }
+        public string cau5_6k_2 { get; set; }
+        public string cau5_7 { get; set; }
+        public string cau5_7k { get; set; }
+        public string cau5_7k_3 { get; set; }
+        public string cau5_8 { get; set; }
+        public string cau5_7k_2 { get; set; }
+        public string tongket { get; set; }
+        public string tuvantiep { get; set; }
+        public string vande { get; set; }
+        public string thoigian { get; set; }
+        public string phiu_t_vn_complete { get; set; }
+    }
+}
