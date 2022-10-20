c1547a8fdebc3d8943b96a19ad47bb84ab1663e0
vũ văn lai <lai.bkhn123@gmail.com>
2022-10-20 21:57:23 +0700

    chỉnh sửa lỗi tìm kiếm báo cáo

--- WebApp/app/Controller/BaoCao6ThangController.js
+++ WebApp/app/Controller/BaoCao6ThangController.js
@@ -85,7 +85,14 @@
         }
 
         if ($scope.ListCityCode != null && $scope.ListCityCode.length > 0) {
-            $scope.modelSearch.CityCodes = $scope.ListCityCode.map(function (obj) { return obj.Code; }).join(',');
+            for (var i = 0; i < $scope.ListCityCode.length; i++) {
+                if ($scope.modelSearch.CityCodes == null || $scope.modelSearch.CityCodes == '') {
+                    $scope.modelSearch.CityCodes = $scope.ListCityCode[i];
+                } else {
+                    $scope.modelSearch.CityCodes += ',' + $scope.ListCityCode[i];
+                }
+            }
+            //$scope.modelSearch.CityCodes = $scope.ListCityCode.map(function (obj) { return obj.Code }).join(',');
         }
         showToast();
         $scope.ListData = [];
