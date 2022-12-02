da9ce35de70c865fd24a93a00427800d04bc7e0d
vũ văn lai <lai.bkhn123@gmail.com>
2022-12-02 10:09:20 +0700

    laivv

--- SyncApp/app/controllers/homecontroller.js
+++ SyncApp/app/controllers/homecontroller.js
@@ -52,7 +52,7 @@
                 type: "Post",
                 url: "/Home/ShowConfig",
                 data: {
-                    name: $scope.process.Code
+                    name: $scope.process.ReportId
                 },
                 success: function (rs) {
                     $scope.logFile = rs.message;
